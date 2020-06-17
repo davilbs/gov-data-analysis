406e4f9efa56dfef051f510704676158532e7f40
davilbs <davi.ativa@gmail.com>
2020-06-17 17:36:18 -0300

    added graph plotting

--- plotgraph.py
+++ plotgraph.py
@@ -1,7 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-natural-2000-2020-m3.csv",
-                     delimiter=";", usecols=(4,9), encoding="utf-8", skip_header=1)
+years = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-natural-2000-2020-m3.csv",
+                     dtype="int", delimiter=";", usecols=(0), encoding="utf-8", skip_header=1)
 
-print(data)
+months = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-natural-2000-2020-m3.csv",
+                     dtype="str", delimiter=";", usecols=(4,5,6,7,8,9,10,11,12,13,14,15), encoding="utf-8", max_rows=1)
+
+data = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-natural-2000-2020-m3.csv",
+                     delimiter=";", usecols=(4,5,6,7,8,9,10,11,12,13,14,15), encoding="utf-8", skip_header=1)
+
+for year in range(len(data)):
+    plt.plot(months, data[year], label=years[year])
+plt.legend(loc="upper right", ncol=3)
+plt.show()
