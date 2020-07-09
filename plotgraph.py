0f350dbd1c797bdfed573545335c208e5f489dff
davilbs <davi.ativa@gmail.com>
2020-07-09 13:02:51 -0300

    changed graph axis

--- plotgraph.py
+++ plotgraph.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scypy import stats
 
 years = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-natural-2000-2020-m3.csv",
                      dtype="int", delimiter=";", usecols=(0), encoding="utf-8", skip_header=1)
@@ -10,7 +11,7 @@ months = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-
 data = np.genfromtxt(fname="importacoes-gas-natural/importacao/importacao-gas-natural-2000-2020-m3.csv",
                      delimiter=";", usecols=(4,5,6,7,8,9,10,11,12,13,14,15), encoding="utf-8", skip_header=1)
 
-for year in range(len(data)):
-    plt.plot(months, data[year], label=years[year])
+for month in range(len(months)):
+    plt.scatter(years, [monthdata[month] for monthdata in data], label=months[month])
 plt.legend(loc="upper right", ncol=3)
 plt.show()
